271c9ddd76f0afe98b9af9c237e7c1d1bd20ddb8
Carlo Rizzardo <cr.git.mail@gmail.com>
2025-09-26 15:49:46 +0200

    Updated installer

--- install_full.sh
+++ install_full.sh
@@ -1,14 +1,105 @@
 #!/bin/bash
 
-if [[ "$1" != "ros1xbot" ]]; then
+# Default values
+prepare_for_ros1xbot=false
+dev_mode=false
+ws_name="adarl_ws"
+verbose=false
+help_requested=false
+
+# Function to display help
+show_help() {
+    echo "Usage: $0 [OPTIONS] [COMMAND]"
+    echo ""
     echo "ADARL AUTOINSTALL SCRIPT"
-    prepare_for_ros1xbot=false
-    ws_name="adarl_ws"
-else
-    echo "ADARL AUTOINSTALL SCRIPT - With ROS1 and XBOT"
-    prepare_for_ros1xbot=true
-    ws_name="adarl_ws_ros1"
+    echo ""
+    echo "COMMANDS:"
+    echo "  ros1xbot          Install with ROS1 and XBOT support"
+    echo ""
+    echo "OPTIONS:"
+    echo "  -d, --dev         Enable development mode"
+    echo "  -v, --verbose     Enable verbose output"
+    echo "  -h, --help        Show this help message"
+    echo "  --ws-name NAME    Set custom workspace name (default: adarl_ws)"
+    echo ""
+    echo "EXAMPLES:"
+    echo "  $0                    # Basic installation"
+    echo "  $0 ros1xbot           # Install with ROS1 and XBOT"
+    echo "  $0 --dev ros1xbot     # Development mode with ROS1 and XBOT"
+    echo "  $0 --ws-name my_ws    # Custom workspace name"
+    echo ""
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -h|--help)
+            help_requested=true
+            shift
+            ;;
+        -d|--dev)
+            dev_mode=true
+            shift
+            ;;
+        -v|--verbose)
+            verbose=true
+            shift
+            ;;
+        --ws-name)
+            if [[ -n "$2" && "$2" != -* ]]; then
+                ws_name="$2"
+                shift 2
+            else
+                echo "Error: --ws-name requires a value"
+                exit 1
+            fi
+            ;;
+        ros1xbot)
+            prepare_for_ros1xbot=true
+            ws_name="adarl_ws_ros1"
+            shift
+            ;;
+        -*)
+            echo "Error: Unknown option $1"
+            show_help
+            exit 1
+            ;;
+        *)
+            echo "Error: Unknown command $1"
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+# Show help if requested
+if [[ "$help_requested" == true ]]; then
+    show_help
+    exit 0
 fi
+
+# Verbose output function
+log_verbose() {
+    if [[ "$verbose" == true ]]; then
+        echo "[VERBOSE] $*"
+    fi
+}
+
+# =============================================================================
+# MAIN SCRIPT LOGIC
+# =============================================================================
+
+if [[ "$prepare_for_ros1xbot" == true ]]; then
+    echo "ADARL AUTOINSTALL SCRIPT - With ROS1 and XBOT"
+else
+    echo "ADARL AUTOINSTALL SCRIPT"
+fi
+
+log_verbose "Configuration:"
+log_verbose "  prepare_for_ros1xbot: $prepare_for_ros1xbot"
+log_verbose "  dev_mode: $dev_mode"
+log_verbose "  ws_name: $ws_name"
+log_verbose "  verbose: $verbose"
 
 echo "This script will:"
 echo " - Create a workspace in your home at ~/$ws_name"
@@ -33,18 +124,33 @@ else
 fi
 ssh-add -t 600
 
-echo "Cloning repositories..."
 sleep 1
-git clone https://github.com/c-rizz/adarl_docker_utils
-git clone git@github.com:ADVRHumanoids/adarl_envs.git
-git clone https://github.com/c-rizz/adarl
-git clone https://github.com/c-rizz/rreal
-git clone --recurse-submodules git@github.com:ADVRHumanoids/pykyon.git
-git clone --recurse-submodules https://github.com/c-rizz/pycentauro
-if [[ prepare_for_ros1xbot ]]; then
-    # git clone https://gitlab.com/crzz/adarl_ros
-    git clone git@github.com:c-rizz/adarl_ros
+if [[ "$dev_mode" == true ]]; then
+    echo "Cloning dev repositories..."
+    git clone https://github.com/c-rizz/adarl_docker_utils.git
+    git clone https://gitlab.com/crzz/adarl_envs.git --branch crzz-dev
+    git clone https://gitlab.com/crzz/adarl.git --branch crzz-dev
+    git clone https://gitlab.com/crzz/rreal.git --branch crzz-dev
+    git clone --recurse-submodules git@github.com:ADVRHumanoids/pykyon.git
+    git clone --recurse-submodules https://github.com/c-rizz/pycentauro
+    if [[ prepare_for_ros1xbot ]]; then
+        # git clone https://gitlab.com/crzz/adarl_ros
+        git clone https://gitlab.com/crzz/adarl_ros.git
+    fi
+else
+    echo "Cloning public repositories..."
+    git clone https://github.com/c-rizz/adarl_docker_utils 
+    git clone git@github.com:ADVRHumanoids/adarl_envs.git
+    git clone https://github.com/c-rizz/adarl
+    git clone https://github.com/c-rizz/rreal
+    git clone --recurse-submodules git@github.com:ADVRHumanoids/pykyon.git
+    git clone --recurse-submodules https://github.com/c-rizz/pycentauro
+    if [[ prepare_for_ros1xbot ]]; then
+        # git clone https://gitlab.com/crzz/adarl_ros
+        git clone git@github.com:c-rizz/adarl_ros
+    fi
 fi
+
 cd ..
 
 if [ stop_sshagent_afterwards ]; then
